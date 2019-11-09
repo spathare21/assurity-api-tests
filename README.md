40df11969db7cbaaa5f19b365500ce2e7b9b7d4e
sachin pathare <in.sachin-pathare@sachins-MacBook-Pro.local>
2019-11-09 20:47:53 +0530

    api tests return for 3 scenarios

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# assurity-api-tests
-API Test
+## assurity-api-tests
+API Test Assignment given by Assurity
+
+### How to use/Run 
+
+- Clone the repo 
+```shell
+$ git clone https://github.com/spathare21/assurity-api-tests.git
+```
+ 
+```shell
+$ cd assurity.api.test
+```
+- Run Below Command to run tests
+```shell
+$  ./gradlew clean test allureReport
+```
+- After completion open reports build/reports/allure-report/index.html
+- You will see the reports like below 
+
+![Reports](https://recordit.co/Eq6UNddQNAf)
+
+### License
+[Apache License](http://www.apache.org/licenses/LICENSE-2.0), Version 2.0
--- src/test/java/com/assurity/tests/FirstTest.java
+++ src/test/java/com/assurity/tests/FirstTest.java
@@ -6,6 +6,10 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
@@ -29,18 +33,38 @@ class FirstTest {
                 .assertThat()
                 .body(matchesJsonSchemaInClasspath("DetailsSchema.json"));
     }
-
     @Test
-    @Story("Verify Name")
-    void testAPIDetails() {
+    @Story("Verify Name Carbon credits is")
+    void testValidName() {
         Response resp = given()
                 .header("Accept", "/")
                 .when().log().all()
                 .get("Categories/6327/Details.json?catalogue=false");
-
-        System.out.println(resp.getBody().jsonPath().get("Name").toString());
-        System.out.println(resp.getBody().jsonPath().get("CanRelist").toString());
-        System.out.println(resp.getBody().jsonPath().get("Promotions").toString());
+        resp.then().assertThat().statusCode(200);
+        assertEquals(resp.getBody().jsonPath().get("Name").toString(),"Carbon credits","Verified Carbon credits found");
+    }
+    @Test
+    @Story("Verify Description of Promotion Gallary ")
+    void promotionGallaryDesctest() {
+        Response resp = given()
+                .header("Accept", "/")
+                .when().log().all()
+                .get("Categories/6327/Details.json?catalogue=false");
+        resp.then().assertThat().statusCode(200);
+        int i = ((ArrayList<String>) (resp.getBody().jsonPath().get("Promotions.Name"))).indexOf("Gallery");
+        String desc = resp.getBody().jsonPath().get("Promotions[" + i + "].Description").toString();
+        System.out.println("Gallary Desc ----------: "+desc);
+        assertThat(desc,containsString("2x larger image"));
+    }
+    @Test
+    @Story("Verify canRelist returned true")
+    void canRelisttest() {
+        Response resp = given()
+                .header("Accept", "/")
+                .when().log().all()
+                .get("Categories/6327/Details.json?catalogue=false");
+        resp.then().assertThat().statusCode(200);
+        assertEquals(resp.getBody().jsonPath().get("CanRelist").toString(),"true");
     }
 
 }
